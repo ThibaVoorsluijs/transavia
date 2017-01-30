aa263e886d3ac2e5eb0484eb56a2b42aed65c9aa
Thiba Voorsluijs <thibavoorsluijs@hotmail.com>
2017-01-30 20:55:31 +0100

    eind, html valide

---
+++ js/content.js
@@ -0,0 +1,126 @@
+var website_content = {
+    "alles": {
+       "boven" : {
+            "titel": "Het gebrek",
+            "footer": "Posted on 29 juni 2011 | by Molovich | 4 Comments",
+            "text" : "De bus uit. Mijn zoon tukt in zijn kinderkoets. Eindelijk. Ik loop over het plein. Een gierende wind en een striemende miezerregen geselen mijn gezicht. De mensen kijken voor zich uit, ieder voor zich. Ik ben op weg richting de bibliotheek om Stijloefeningen te lenen van hoe heet die schijver, iets met een Q. Welk een bende is het hier trouwens, zie ik nu! Ik moet verder over een stuk loophout. Beneden mij bevinden zich diepe bouwputten. Mijn trommelvliezen worden op hun degelijkheid getest door heiende heipalen en drillende drilboren. Ik mis iets. Ik voel het. Het klopt niet. Ik mis iets."
+        },
+        "onder1" : {
+            "titel": "Kinderboek",
+            "footer": "Posted on 27 mei 2011 | by Molovich | 1 Comment ",
+            "text": "Kijk, daar gaat Max. Dag Max! Hij stapt uit de bus. Wat duwt hij daar vooruit? Jawel, een heuse kinderwagen! En wat voor één! Een groene! Zijn zoontje ligt er in te slapen. Die heeft gelukkig geen last van de kou. Want het is koud joh! Het regent een beetje en het waait flink. Alle mensen op het stationsplein kijken naar de grond. Ze hebben hun hoofd diep in hun kraag gestoken. Max is op weg naar de bibliotheek om een boek te lenen. Een boek waarin 99 keer hetzelfde verhaal wordt verteld. Wat is dat een eigenaardig boek, zeg! Die schrijver was zeker met zijn hoofd in een bankschroef terecht gekomen? Maar even iets anders: wat een rotzooi is het hier! Max moet over een smalle, modderige plank lopen. Overal zijn ze aan het bouwen! Links van de plank is er een diepe put. Rechts van de plank is er een diepe put. Heel ver beneden zijn kleine mannetjes met gele helmpjes druk bezig om grote gebouwen te bouwen. Beukende heipalen en drillende drilboren banen zich een weg naar het oor van de arme Max.",
+            "icon": "images/afb_kinderboek.png"
+        },
+        "onder2" : {
+            "titel": "Zombie",
+            "footer": "Posted on 21 juni 2011 | by Molovich | Leave a comment ",
+            "text": "Duizenden hongerige ondoden lopen zielloos over het stationsplein. Op zoek naar voedsel, naar half vergane hondenlijken, naar vers mensenvlees. Het is gekkenwerk dat ik hier ben, maar de brandstof van de bus is op. Ik hoopte dwars door de menigte wandelende lijken heen te kunnen rauzen. Maar het mocht niet zo zijn. Ergens ter hoogte van het gebouw dat ooit Oibibio huisvestte, hield de bus op met rijden. Ik heb ‘m uit laten rollen en nu loop ik hier. Onder mijn gele regenpak heb ik mezelf in aluminiumfolie verpakt in de hoop niet besmet te worden door de verontreinigde miezerregen. Donkere wolken hangen als een woedende God boven het troosteloze Centraal Station. Een venijnige wind rammelt aan de kapotte ramen. De kinderwagen die ik voortduw zit vol met jachtgeweren, ijzeren staven, keukenmessen, een shotgun en een kettingzaag. Maar als ik dat hier moet gebruiken… als ze merken dat ik nog niet dood ben… dan ben ik verloren, dat weet ik maar al te goed. Waarom doe ik dit eigenlijk? Om Stijloefeningen van Raymond Queneau te kunnen bemachtigen. Tja. Als ik mij iets in m’n hoofd haal, dan rust ik niet voordat ik het voor elkaar heb. Zo ben ik. Daar kan geen bloeddorstige ondode ook maar iets aan veranderen. Gelukkig zijn ze te veel met zichzelf bezig om me op te merken. Ik heb het gehaald en loop nu over een plank. Links en rechts modderige bouwputten die er verlaten bij liggen. Beneden vechten drie schurftige honden om een groen uitgeslagen mensenbeen.",
+            "icon": "images/afb_zombie.png"
+        }
+    },
+
+    "drama": {
+       "boven" : {
+            "titel": "Paranoia",
+            "footer": "Posted on 21 april 2011 | by    Molovich | 3 Comments",
+            "text" : "Ik sluip de bus uit. Kop in de kraag. Ik maak me klein en duw m’n zoon vooruit. Hij slaapt. Eindelijk. Zijn gejengel trok veel te veel aandacht. De mensen lopen over het stationsplein. Het voelt alsof er een grote, knipperende pijl boven m’n hoofd hangt: HIER LOOPT HIJ! Gelukkig is het kloteweer en kijken de mensen vooral naar de grond. Ik moet naar de Openbare Bibliotheek om het laatste stuk wrakhout te vinden waar ik mij aan kan vastklampen: Stijloefeningen van Raymond Queneau. Twee politieagenten te fiets staan een praatje te maken. Ik moet er langs. Zo rustig mogelijk. De mensen achter me. Ik voel hun ogen. Amsterdam ligt open hier. Een grote, gapende, open wond. Ik moet me losmaken uit de massa en wandel over een plank met aan weerszijden bouwputten. Drilboren en heipalen overstemmen mijn piepende ademhaling. Een delegatie Japanse toeristen moet er langs. Ze hebben allemaal van die driehoekige posterdozen van het Van Gogh Museum bij zich. Maar zitten er wel posters in? Angstzweet gutst langs m’n slapen."
+        },
+        "onder1" : {
+            "titel": "Vrijdag de 13e",
+            "footer": "Posted on 13 mei 2011 | by Molovich | Leave a comment",
+            "text": "Bij het uitstappen van de bus waait mijn jas open, precies op het moment dat de buschauffeur de deuren sluit. Terwijl ik de kinderwagen waarin mijn zoon ligt te krijsen vasthoudt, ren ik mee met de bus en schreeuw ik naar de buschauffeur dat hij moet stoppen. Maar hij hoort me niet, de bus draait de bocht om en ik scheur uit mijn jas. Met een halve jas trotseer ik een venijnig windje en een gemene miezerregen. De mensen kijken naar beneden, kop in de kraag. Ik kijk fier vooruit, meen te ruiken dat mijn zoon heeft gepoept, kom met mijn schoen in de tramrails vast te zitten en verzwik mijn rechtervoet. Ik strompel door. Op naar de Openbare Bibliotheek om Stijloefeningen van Raymond Queneau te lenen. Ben ik al een week of wat naar op zoek, maar het is nergens te vinden. Wat een chaos is het hier trouwens rond Centraal Station! Overal modderige bouwputten. Een windvlaag tilt mijn hoed op en brengt ’m naar beneden waar hij in het natte cement van een fundering blijft liggen.",
+            "icon": "images/verhaal_vrijdag13e.png"
+        },
+        "onder2" : {
+            "titel": "Blind",
+            "footer": "Posted on 3 juni 2011 | by Molovich | Leave a comment",
+            "text": "Piepende remmen, bus 21 komt tot stilstand. De stem van de buschauffeur, naar mij gericht, zakelijk, niet vriendelijk, niet onvriendelijk: ‘Centraal Station, eindhalte’. Het sissende geluid van busdeuren die open gaan. Mensen drommen naar buiten. Voorzichtig vind ik mijn weg naar de uitgang en stap uit. Eigenlijk wilde ik mijn zoon meenemen, maar dat mocht niet van m’n vrouw. ‘Je bent blind, gek’, zei ze, ‘wat denk je nu, dat je de kinderwagen als blindegeleidehond kan gebruiken?’ Ik ben op zoek naar Stijloefeningen van Raymond Queneau in de vertaling van Rudy Kousbroek. Het liefst als luisterboek of in braille. Maar als dat er niet is, vind ik wel iemand om het voor te lezen. Ik zoek het al een tijdje, maar het is nergens te vinden. Vandaar dat ik naar de Openbare Bibliotheek te Amsterdam ga om ‘m te lenen. Auto’s suizen langs. ",
+            "icon": "images/verhaal_blind.png"
+        }
+    },
+
+    "kritisch": {
+       "boven" : {
+            "titel": "Verrast",
+            "footer": "Posted on 5 februari 2011 | by Molovich | 6 Comments",
+            "text" : "Precies op het moment dat ik de bus uit stap valt mijn zoon in slaap! Wat een timing, ongefuckinglooflijk! Gelukkig slaapt hij vrij diep, want wat een chaos op het Centraal Station zeg! Overal opgebroken wegen en borende wegwerkers en heiende heipalen! Ik loop naar de Openbare Bibliotheek langs de gigantische bouwputten. Wat een desolate sfeer hier trouwens! De wind snijdt dwars door mijn jas heen! Als ik eindelijk de bibliotheek bereik, klampt een vrouw mij aan! (Een vrouw!) (Met zwarte krullen!) Ze vraagt waar of dat het postkantoor is. Welke wereldvreemde gek heeft haar verteld dat ze hier moet zijn?! Dat postkantoor staat hier toch al zeker tien jaar niet meer! Ik vertel haar dat ze naar de Raadhuisstraat moet. En dan – een auto rijdt weg, de vrouw kijkt geschrokken, ze twijfelt. Aan de nummerplaat te zien komen ze helemaal uit Frankrijk! De vrouw zwaait, maar de bestuurder ziet haar niet! En dan zet ze het op een lopen! Wat een waanzinnige zwarte krullen dansen er op haar hoofd!"
+        },
+        "onder1" : {
+            "titel": "Kamervragen van Richard de Mos",
+            "footer": "Posted on 21 februari 2011 | by Molovich | 1 Comment",
+            "text": "Voorzitter, 1. Heeft de Minister het bericht op Nurks van Max Molovich gelezen waarin hij vertelt dat hij met bus 21 naar het Centraal Station gaat. Zo ja, is de Minister zich ervan bewust zijnde dat de omgeving rond het Centraal Station eruit ziet als een slagveld uit de Eerste Wereldoorlog? En is de Minister er zich van bewust dat dit ten grondslag ligt aan de Noord-Zuidlijn? Zo ja, wat denkt de minister hier aan te kunnen gaan doen?",
+            "icon": "images/verhaal_kamervragen.png"
+        },
+        "onder2" : {
+            "titel": "Hyperbool",
+            "footer": "Posted on 11 februari 2011 | by Molovich | 4 Comments",
+            "text": "Ik stap de bus uit. De mensen stonken weer eens een uur in de wind. Stelletje bunzingen. Ik ben met mijn zoon en laveer met zijn kinderwagen tussen de miljarden mensen die het nodig achten de trein te nemen. Wat een verschrikking. Het is dat ik niet anders kon. Ik ben op zoek naar Stijloefeningen van Raymond Queneau, de grootste schrijver aller tijden. Ben ik al minstens een eeuw naar op zoek. Maar nergens te vinden. Als laatste sterft de hoop, en die hoop is gevestigd op de Openbare Bibliotheek van Amsterdam. Maar voor ik daar ben, moet ik mij een weg banen door deze oceaan van mensen. Kunnen ze hier niet eens een bom op gooien? Bij voorkeur als ik er niet ben.",
+            "icon": "images/verhaal_hyperbool.png"
+        }
+
+    },
+
+    "poezie": {
+       "boven" : {
+            "titel": "Vrije versvorm",
+            "footer": "Posted on 25 februari 2011 | by Molovich | Leave a comment",
+            "text" : "Gemeenteblik<br> Draaiende wielen stoppen<br> Schimmen lopen van en naar<br> Zoonlief droomt<br> Over wat dan ook Het miezert en"
+        },
+        "onder1" : {
+            "titel": "Song (Je vindt nooit wat je zoekt)",
+            "footer": "Posted on 6 april 2011 | by    Molovich | 8 Comments",
+            "text": "Je vindt nooit wat je zoekt (Muziek: George L. River, tekst: Max J. Molovich) Ik stap uit de bus, samen met m’n zoon",
+            "icon": "images/verhaal_song.png"
+        },
+        "onder2" : {
+            "titel": "Sonnet",
+            "footer": "Posted on 1 maart 2011 |    by    Molovich | 7 Comments",
+            "text": "Bus 21, 12 uur, begin van de week Mijn zoon slaapt in zijn wagentje, en zo Ga ik op zoek naar Stijloefeningen van Queneau",
+            "icon": "images/verhaal_sonnet.png"
+        }
+
+    },
+
+    "romantiek": {
+       "boven" : {
+            "titel": "Verliefd",
+            "footer": "Posted on 14 februari 2011 | by Molovich | 2 Comments",
+            "text" : "Licht van mijn leven. Ik stapte vandaag bus 21 uit. Het was op het midden van de dag. En wat een heerlijke dag! Het was twaalf uur, onze fantastische zoon sliep net. De miezelregen aaide de haartjes op mijn hand en ik dacht aan jou. De mensen dansten en huppelden van en naar het station, en ik dacht aan jou. Ik was op weg naar de bibliotheek in de hoop Stijloefeningen van Raymond Queneau te vinden, en ik dacht aan jou. Je was er niet, maar ik dacht aan jou. En zo was jij er toch bij. Onze zoon sliep. Had ik dat al gezegd? Ja dat had ik al gezegd, maar wat doet het er toe, want ik dacht aan jou. Niets doet er toe, zo lang ik maar aan jou kan denken. Ik liep zingend over de planken, langs de bouwputten. De bouwvakkers beneden zongen met mij mee. Wij zongen voor jou."
+        },
+        "onder1" : {
+            "titel": "Droom",
+            "footer": "Posted on 4 februari 2011 | by Molovich | 3 Comments",
+            "text": "Slierten mist zwerven rond mijn benen. Net zat ik nog in Bus 21. Het was warm, zweet parelde langs mijn slapen, mijn zoon sliep. Nu loop ik over een smal pad langs eindeloos diepe bouwputten. Een heipaal beukt zich een weg naar het binnenste van mijn oor. Lachende Japanners passeren mij. Een van de Japanners verandert in een vrouw. ‘Waar is het postkantoor,’ vraagt de vrouw. ‘Dat bestaat niet meer,’ zeg ik. ‘We werken hier alleen nog maar met postduiven.’ Een auto met een Frans nummerbord en op het dak een opblaasbare Smurf met een enorme penis rijdt achteruit. De vrouw rent er achteraan. De auto en de rennende vrouw worden kleiner en kleiner. Ik kijk voor me. Daar staat het postkantoor.",
+            "icon": "images/verhaal_droom.png"
+        },
+        "onder2" : {
+            "titel": "Nederlands toneel",
+            "footer": "Posted on 4 april 2011 | by Molovich | 2 Comments",
+            "text": "Scene 1: Op het toneel staat een paal met bovenop een rond bord waarop we BUSSTOP lezen. Achter het bord hangt een groot wit doek, waarop filmbeelden van het Amsterdamse Centraal Station zijn geprojecteerd. Mensen lopen af en aan. Het toneel is verder leeg. Een man (‘onze held’) komt aangelopen. Op het filmdoek zien we een bus. Onze held loopt een beetje eigenaardig want hij doet net of hij zit. Bij de BUSSTOP aangekomen stopt hij en stapt hij de denkbeeldige bus uit. Hij duwt een trolley voort (zo eentje waarmee bejaarden boodschappen doen). Onze held begint te praten, schijnbaar in het niets. Met luide stem.",
+            "icon": "images/verhaal_toneel.png"
+        }
+
+    },
+
+    "sport": {
+       "boven" : {
+            "titel": "Voetbalanalisten 1",
+            "footer": "Posted on 29 april 2011 | by Molovich | 5 Comments",
+            "text" : "Jack van Gelder: Jongens, we hebben net gekeken naar Max Molovich die, samen met zijn slapende zoon op zoek ging naar Stijloefeningen van Queneau. Wat vonden we ervan?"
+        },
+        "onder1" : {
+            "titel": "Johan Cruijff",
+            "footer": "Posted on 18 februari 2011 | by Molovich | 6 Comments",
+            "text": "In principe is het dus zo dat je uit Bus 21 stapte en op weg was naar de Openbare Bibliotheek om Stijloefeningen van Raymond Queneau te lenen. Je zocht er dus zeg maar al een tijdje naar, maar het was nergens te vinden, dus daarom moest het wel in de bibliotheek te vinden zijn. En als het er toch niet is, dan ben je dus zeg maar of te vroeg vertrokken, of te laat.",
+            "icon": "images/verhaal_johan.png"
+        },
+        "onder2" : {
+            "titel": "Sportcommentaar",
+            "footer": "Posted on 28 april 2011 | by Molovich | 2 Comments",
+            "text": "En daar stopt lijn 21 op exact de juiste tijd op precies de juiste plek op het Stationsplein. De deuren gaat open, de spanning is te snijden. En wie komt daar als eerste uit? Ja hoor, het is de felgroene kinderwagen waarin Molovich Jr. ligt te slapen, op de voet gevolgd door zijn vader. De overige passagiers hebben het nakijken, vader en zoon Molovich stuiven er vandoor en hebben al snel een straatlengte voorsprong.",
+            "icon": "images/verhaal_sportcommentaar.png"
+        }
+
+    }
+
+}
--- js/index.js
+++ js/index.js
@@ -6,93 +6,134 @@ Siebe Domeijer
 
 =================== */
 
+/* =================
+Opties en categorieën
+================  */
 
-//voor mobiel
-function optiesButtonClick() {
-    var optiesForm =  document.getElementById('opties-mobiel');
-    var categorieSection =  document.getElementById('categorie-section');
-    if ( optiesForm.style.display == "flex" ) {
-        optiesForm.style.display = "";
-    } else {
-        optiesForm.style.display = "flex";
-        categorieSection.style.display = "";
-    }
-}
+//meer opties voor mobiel
+document.querySelector('body > section > button:first-of-type').addEventListener("click", function () {
+    document.querySelector('body > section > form').classList.toggle("visible-height");
+    document.querySelector('body > section > section').classList.remove("visible-height");
+});
 
-//voor mobiel
-function categorieButtonClick() {
-    var optiesForm =  document.getElementById('opties-mobiel');
-    var categorieSection =  document.getElementById('categorie-section');
-    if ( categorieSection.style.display == "flex" ) {
-        categorieSection.style.display = "";
-    } else {
-        categorieSection.style.display = "flex";
-        optiesForm.style.display = "";
-    }
-}
+//categorieen voor mobiel
+document.querySelector('body > section > button:last-of-type').addEventListener("click", function () {
+    document.querySelector('body > section > section').classList.toggle("visible-height");
+    document.querySelector('body > section > form').classList.remove("visible-height");
+});
 
-//voor desktop en tablet
-function meerMinderOptiesButtonClick () {
-    var optiesForm =  document.getElementById('opties-mobiel');
-    var optiesButton =  document.getElementById('meer-minder-opties');
-    if ( optiesForm.style.left == "0rem" ) {
-        optiesForm.style.left = "";
-        optiesButton.style.left = "";
-        optiesButton.style.background = ""
-        optiesButton.style.backgroundSize = "";
-    } else {
-        optiesForm.style.left = "0rem";
-        optiesButton.style.left = "19rem";
-        optiesButton.style.background = "url(images/minder_opties.png) center no-repeat"
-        optiesButton.style.backgroundSize = "contain";
-    }
 
-}
+//Meer opties voor desktop en tablet
+document.querySelector('body > section > button:nth-of-type(2)').addEventListener("click", function () {
+    document.querySelector('body > section > button:nth-of-type(2)').classList.toggle("shift-right");
+    document.querySelector('body > section > form').classList.toggle("shift-right");
+});
 
 //als er buiten een button wordt geklikt, worden alle style waarden gereset
 window.onclick = function(event) {
   if (!event.target.matches('body > section > button') &&
-      !event.target.matches('body > section > form  *')
-    ) {
-
-    var optiesForm =  document.getElementById('opties-mobiel');
-    var categorieSection =  document.getElementById('categorie-section');
-    var optiesButton =  document.getElementById('meer-minder-opties');
-
-    optiesForm.style.display = "";
-    optiesForm.style.left = "";
-    optiesButton.style.left = "";
-    optiesButton.style.background = ""
-    optiesButton.style.backgroundSize = "";
-    categorieSection.style.display = "";
+      !event.target.matches('body > section > form  *'))
+  {
+    resetClasses()
   }
 }
 
-//als window veranderd van grootte worden alle style waarden gereset
+//als window veranderd van grootte worden alle style waarden gereset (voor het testen)
 window.onresize = function () {
-    var optiesForm =  document.getElementById('opties-mobiel');
-    var categorieSection =  document.getElementById('categorie-section');
-    var optiesButton =  document.getElementById('meer-minder-opties');
-
-    optiesForm.style.display = "";
-    optiesForm.style.left = "";
-    optiesButton.style.left = "";
-    optiesButton.style.background = ""
-    optiesButton.style.backgroundSize = "";
-    categorieSection.style.display = "";
+    resetClasses()
 }
+
+function resetClasses() {
+    document.querySelector('body > section > form').classList.remove("visible-height");
+    document.querySelector('body > section > section').classList.remove("visible-height");
+    document.querySelector('body > section > button:nth-of-type(2)').classList.remove("shift-right");
+    document.querySelector('body > section > form').classList.remove("shift-right");
+}
+
+
+/* =================
+Scroll functie en button
+================  */
 
 
 //http://stackoverflow.com/questions/3464876/javascript-get-window-x-y-position-for-scroll
-//verberg de terug-naar-boven knop bij de eerster 200 pixels
+//verberg de terug-naar-boven knop bij de eerster 400 pixels (dit moet in pixels)
 window.onscroll = function() {
-    var terugNaarBovenAnchor = document.getElementById('terug-naar-boven')
+    var terugNaarBovenAnchor = document.querySelector('footer > button');
     if (window.pageYOffset > 400) {
         // > 100px from top - show div
-        terugNaarBovenAnchor.style.display = "block"
+        terugNaarBovenAnchor.classList.add("shift-up");
     }
     else {
-        terugNaarBovenAnchor.style.display = ""
+        terugNaarBovenAnchor.classList.remove("shift-up");
     }
 }
+
+//click op de terug-naar boven knop, start de ScrollTo functie, 250 is ms
+document.querySelector('footer > button').addEventListener("click", function () {
+    scrollTo(
+        document.body,
+        document.querySelector('header'),
+        250
+    )
+});
+
+//http://stackoverflow.com/a/31987330
+function scrollTo(element, to, duration) {
+    if (duration <= 0) return;
+    var difference = to - element.scrollTop;
+    var perTick = difference / duration * 10;
+
+    setTimeout(function() {
+        element.scrollTop = element.scrollTop + perTick;
+        if (element.scrollTop === to) return;
+        scrollTo(element, to, duration - 10);
+    }, 10);
+}
+
+
+/* =================
+Content selectie
+================  */
+
+function setWebSiteContent(categorie) {
+
+        document.querySelector('main > section:first-of-type > article > h1').innerHTML = website_content[categorie]['boven']['titel'];
+        document.querySelector('main > section:first-of-type > article > footer').innerHTML = website_content[categorie]['boven']['footer'];
+        document.querySelector('main > section:first-of-type > article > p').innerHTML = website_content[categorie]['boven']['text'];
+
+        document.querySelector('main > section:last-of-type > article:first-of-type > h2').innerHTML = website_content[categorie]['onder1']['titel'];
+        document.querySelector('main > section:last-of-type > article:first-of-type > footer').innerHTML = website_content[categorie]['onder1']['footer'];
+        document.querySelector('main > section:last-of-type > article:first-of-type > p').innerHTML = website_content[categorie]['onder1']['text'];
+        document.querySelector('main > section:last-of-type > article:first-of-type > img').src = website_content[categorie]['onder1']['icon'];
+
+        document.querySelector('main > section:last-of-type > article:nth-of-type(2) > h2').innerHTML = website_content[categorie]['onder2']['titel'];
+        document.querySelector('main > section:last-of-type > article:nth-of-type(2) > footer').innerHTML = website_content[categorie]['onder2']['footer'];
+        document.querySelector('main > section:last-of-type > article:nth-of-type(2) > p').innerHTML = website_content[categorie]['onder2']['text'];
+        document.querySelector('main > section:last-of-type > article:nth-of-type(2) > img').src = website_content[categorie]['onder2']['icon'];
+}
+
+document.querySelector('body > section > section > ul > li:nth-of-type(1)').addEventListener("click", function () {
+    setWebSiteContent('alles');
+});
+
+document.querySelector('body > section > section > ul > li:nth-of-type(2)').addEventListener("click", function () {
+    setWebSiteContent('drama');
+});
+
+document.querySelector('body > section > section > ul > li:nth-of-type(3)').addEventListener("click", function () {
+    setWebSiteContent('kritisch');
+});
+
+document.querySelector('body > section > section > ul > li:nth-of-type(4)').addEventListener("click", function () {
+    setWebSiteContent('poezie');
+});
+
+document.querySelector('body > section > section > ul > li:nth-of-type(5)').addEventListener("click", function () {
+    setWebSiteContent('romantiek');
+});
+
+document.querySelector('body > section > section > ul > li:nth-of-type(6)').addEventListener("click", function () {
+    setWebSiteContent('sport');
+});
 
